fca49b352516ce24a501ecd1d2576e855f400bce
Xin-Ping Chen <xin-pingchen@Xin-Pings-MacBook-Air.local>
2013-12-25 21:27:37 +0800

    [AUTO HEROKU DEPLOY]

--- index.php
+++ index.php
@@ -39,19 +39,6 @@
 								<li><a href="http://calm-scrubland-1840.herokuapp.com/Friends.html">Friends'</a></li>
 								<li><a href="http://calm-scrubland-1840.herokuapp.com/Ranks.html">Rank</a></li>
 								<li><a href="http://calm-scrubland-1840.herokuapp.com/Catagory.html">Catagory</a></li>
-								<!--<li class="dropdown">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-								<ul class="dropdown-menu">
-									<li><a href="#">Action</a></li>
-									<li><a href="#">Another action</a></li>
-									<li><a href="#">Something else here</a></li>
-									<li class="divider"></li>
-									<li class="dropdown-header">Nav header</li>
-									<li><a href="#">Separated link</a></li>
-									<li><a href="#">One more separated link</a></li>
-								</ul>
-								</li>
-								<-->
 							</ul>
 						</div>
 					</div>
@@ -72,7 +59,7 @@
 							<h3><font color="white">XX </font></h3>   
 							<p class="lead">
 							</p><div class="alert alert-info" style="opacity: 0.7"><font color="black" size="3">
-									I am currently an Assistant Professor in the Department of Computer Science at University of Taipei.  I received my Ph.D. degree from National Taiwan University in 2011. My research interest is computational finance, which is the intersection among three disciplines: computer science, financial economics, and mathematics. Recently, I am also interested in topics related to machine learning in computational finance.</font>
+									I am XX</font>
 							</div>
 							<p></p>
 						</div>
@@ -86,7 +73,7 @@
 							<h3><font color="white">XX </font></h3>   
 							<p class="lead">
 							</p><div class="alert alert-error" style="opacity: 0.7"><font color="black" size="3">
-									I am currently an Assistant Professor in the Department of Computer Science at University of Taipei.  I received my Ph.D. degree from National Taiwan University in 2011. My research interest is computational finance, which is the intersection among three disciplines: computer science, financial economics, and mathematics. Recently, I am also interested in topics related to machine learning in computational finance.</font>
+									I am XX</font>
 							</div>
 							<p></p>
 						</div>
@@ -100,7 +87,7 @@
 							<h3>XX </h3>   
 							<p class="lead">
 							</p><div class="alert alert-success" style="opacity: 0.7"><font color="black" size="3">
-									I am currently an Assistant Professor in the Department of Computer Science at University of Taipei.  I received my Ph.D. degree from National Taiwan University in 2011. My research interest is computational finance, which is the intersection among three disciplines: computer science, financial economics, and mathematics. Recently, I am also interested in topics related to machine learning in computational finance.</font>
+								I am XX</font>
 							</div>
 							<p></p>
 						</div>
@@ -113,294 +100,13 @@
 			<a class="left carousel-control" href="http://calm-scrubland-1840.herokuapp.com/#myCarousel" data-slide="prev">‹</a>
 
 		</div>
-			<!--- /.carousel --->
+			<!-- /.carousel -->
 
 
 			<!-- Marketing messaging and featurettes
 			================================================== -->
 			<!-- Wrap the rest of the page in another container to center all the content. -->
 
-			<div class="container marketing">
-				<!-- Three columns of text below the carousel -->
-
-				<!-- START THE FEATURETTES -->
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-7">
-						<h2 class="featurette-heading"> You may like...  <span class="text-muted">It'll blow your mind!</span></h2>
-						<p class="lead"> all kinds of food </p>
-					</div>
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Burgers <span class="text-muted">See for yourself.</span></h2>
-						<p class="lead">burgers, fries, bagles... </p>
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Steak <span class="text-muted">Check it out! </span></h2>
-						<p class="lead"> </p>
-					</div>
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Pizza <span class="text-muted">See it yourself.</span></h2>
-						<p class="lead"> </p>
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Pasta <span class="text-muted">Check it out! </span></h2>
-						<p class="lead"> </p>
-					</div>
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Salad <span class="text-muted">See for yourself.</span></h2>
-						<p class="lead"> </p>
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Brunch <span class="text-muted">Check it out! </span></h2>
-						<p class="lead"> </p>
-					</div>
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Dessert <span class="text-muted">See for yourself.</span></h2>
-						<p class="lead"> </p>
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Nightmarket food <span class="text-muted">Check it out! </span></h2>
-						<p class="lead"> </p>
-					</div>
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-				</div>
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Hot Pot <span class="text-muted">See for yourself.</span></h2>
-						<p class="lead"> </p>
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Sukiyaki <span class="text-muted">Check it out! </span></h2>
-						<p class="lead"> </p>
-					</div>
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Barbeque <span class="text-muted">See for yourself.</span></h2>
-						<p class="lead"> </p>
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Japanese food <span class="text-muted">Check it out! </span></h2>
-						<p class="lead">Sashimi susi big_bang_hot la_noodles hole_rise</p>
-					</div>
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Japanese pork steak <span class="text-muted">See for yourself.</span></h2>
-						<p class="lead"> </p>
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Korean food <span class="text-muted">Check it out! </span></h2>
-						<p class="lead"> </p>
-					</div>
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Hong Kong food <span class="text-muted">See for yourself.</span></h2>
-						<p class="lead"> </p>
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Iron borad hot <span class="text-muted">Check it out! </span></h2>
-						<p class="lead"> </p>
-					</div>
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Goat Meet Pot &amp; Ginger Duck Mom <span class="text-muted">See for yourself.</span></h2>
-						<p class="lead"> </p>
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Tailand food <span class="text-muted">Check it out! </span></h2>
-						<p class="lead"> </p>
-					</div>
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Sorth Pacific food <span class="text-muted">See for yourself.</span></h2>
-						<p class="lead"> </p>
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-7">
-						<h2 class="featurette-heading">Mexico food <span class="text-muted">Check it out! </span></h2>
-						<p class="lead"> </p>
-					</div>
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-					<div class="col-md-7">
-						<h2 class="featurette-heading">German food <span class="text-muted">See for yourself.</span></h2>
-						<p class="lead"> </p>
-					</div>
-				</div>
-
-				<hr class="featurette-divider">
-
-				<div class="row featurette">
-					<div class="col-md-7">
-						<h2 class="featurette-heading">French food <span class="text-muted">Check it out! </span></h2>
-						<p class="lead"> </p>
-					</div>
-					<div class="col-md-5">
-						<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-					</div>
-				</div>
-
-				<!-- /END THE FEATURETTES -->
 
 
 				<!-- FOOTER -->
